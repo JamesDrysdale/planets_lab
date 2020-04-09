b6f29e81d1fda4472f3f5d58d64f981992e37ab3
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-04-09 15:14:03 +0100

    find largest planet passes testing

--- star_system.rb
+++ star_system.rb
@@ -6,7 +6,6 @@ class StarSystem
     @planets = planets
   end
 
-
   def planet_names()
     return @planets.map { |planet| planet.name }
   end
@@ -16,26 +15,29 @@ class StarSystem
     return planet_name
   end
 
-def get_largest_planet()
- planet_sizes = @planets.map { |planet| planet.diameter }
+  def get_largest_planet()
+    planet_sizes = @planets.map { |planet| planet.diameter }
+    #p planet_sizes.max
+    return @planets.find{|planet| planet.diameter == planet_sizes.max}
+    # for planet in @planets
+    #   if(planet.diameter == planet_sizes.max)
+    #     return planet
+    #   end
+    # end
+  end
 
- p planet_sizes.max
- return planet_sizes
-end
+  def get_planets_with_no_moons()
+    planet_name = @planets.find_all { |planet| planet.number_of_moons == 0 }
+    return planet_name
+  end
 
-def get_planets_with_no_moons()
-  planet_name = @planets.find_all { |planet| planet.number_of_moons == 0 }
-  return planet_name
-end
+  def get_planets_with_more_moons(moons)
+    planet_name = @planets.find_all { |planet| planet.number_of_moons > moons }
+    return planet_name.map { |planet| planet.name }
+  end
 
-def get_planets_with_more_moons(moons)
-  planet_name = @planets.find_all { |planet| planet.number_of_moons > moons }
-  return planet_name.map { |planet| planet.name }
-end
-
-def get_number_of_planets_closer_than(distance)
-  planet_name = @planets.find_all { |planet| planet.distance_from_sun < distance }
-  return planet_name.length
-end
-
+  def get_number_of_planets_closer_than(distance)
+    planet_name = @planets.find_all { |planet| planet.distance_from_sun < distance }
+    return planet_name.length
+  end
 end
